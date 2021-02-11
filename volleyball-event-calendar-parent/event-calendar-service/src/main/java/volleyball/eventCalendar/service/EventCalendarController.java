5723b5fc08788aa441f3497e5b1919636e55e307
Sebastian Büge <sebastian_buege@gmx.de>
2021-02-11 16:33:11 +0100

    fix project structure

--- volleyball-event-calendar-parent/event-calendar-service/src/main/java/volleyball/eventCalendar/service/EventCalendarController.java
+++ volleyball-event-calendar-parent/event-calendar-service/src/main/java/volleyball/eventCalendar/service/EventCalendarController.java
@@ -2,12 +2,11 @@ package volleyball.eventCalendar.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import volleyball.eventCalendar.factory.samsFactory.SamsFactory;
-import volleyball.eventCalendar.service.configuration.CSVConfiguration;
 import volleyball.eventCalendar.service.configuration.Configuration;
+import volleyball.eventCalendar.tools.csvparser.CSVParser;
 import volleyball.tools.downloader.FileDownloader;
 
 import java.net.MalformedURLException;
