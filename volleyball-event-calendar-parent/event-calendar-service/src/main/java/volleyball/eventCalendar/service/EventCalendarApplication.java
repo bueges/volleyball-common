71108482573f40df942c626b19710403a7090919
Sebastian Büge <sebastian_buege@gmx.de>
2021-02-15 21:19:16 +0100

    add spring boot configuration

--- volleyball-event-calendar-parent/event-calendar-service/src/main/java/volleyball/eventCalendar/service/EventCalendarApplication.java
+++ volleyball-event-calendar-parent/event-calendar-service/src/main/java/volleyball/eventCalendar/service/EventCalendarApplication.java
@@ -2,10 +2,16 @@ package volleyball.eventCalendar.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.persistence.Entity;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"volleyball.eventCalendar", "volleyball.repository"})
+@EnableJpaRepositories(basePackages = "volleyball.repository")
+@EntityScan("volleyball.model")
 public class EventCalendarApplication {
 
     public static void main(String[] args) {
