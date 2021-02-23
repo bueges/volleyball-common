6592750840c54a71a97d382266c5991d7c607f4e
Sebastian Büge <sebastian_buege@gmx.de>
2021-02-23 17:08:33 +0100

    add class diagramm

--- volleyball-event-calendar-parent/event-calendar-service/src/main/java/volleyball/eventCalendar/service/EventCalendarApplication.java
+++ volleyball-event-calendar-parent/event-calendar-service/src/main/java/volleyball/eventCalendar/service/EventCalendarApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import javax.persistence.Entity;
-
 @SpringBootApplication
 @ComponentScan(basePackages = {"volleyball.eventCalendar", "volleyball.repository"})
 @EnableJpaRepositories(basePackages = "volleyball.repository")
